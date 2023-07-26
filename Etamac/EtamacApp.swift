14e606f3783fdffae9be15598b39199c0bb8c5e2
Rohan Jain <rjain@imsa.edu>
2023-07-25 21:01:19 -0500

    Initial Commit

---
+++ Etamac/EtamacApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtamacApp.swift
+//  Etamac
+//
+//  Created by rohan jain on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct EtamacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
